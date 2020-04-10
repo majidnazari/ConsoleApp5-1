e5032324d8437fe441cfd390c65743a7e51a119e
majid <majid@DESKTOP-US64O5C>
2020-04-10 13:49:57 +0430

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Hello World!");
+            }
+           
+        }
+    }
+}
